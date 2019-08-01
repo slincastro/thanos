bc1545f770eacb91b42650665e19b25e262b11f4
Denisse <denisse@Denisses-MacBook-Pro.local>
2019-08-01 18:51:10 -0500

    Changes path to use the directory as project

--- gamora/upload_firmware.sh
+++ gamora/upload_firmware.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 USER=$(id -un)
-BASE_PATH="/Users/$USER/Projects/esp8266-dependencies/"
+BASE_PATH=$(pwd)'/esp8266-dependencies/'
 FIRMWARE_PATH=$BASE_PATH'esp8266-Frirmware.bin'
 FIRMWARE_URL='https://micropython.org/resources/firmware/esp8266-20190529-v1.11.bin'
 ESP8266_PORT=$(ls /dev/tty.* | grep "USB")
@@ -14,7 +14,7 @@ echo "USER :" $USER
 echo "ESP PORT :"$ESP8266_PORT
 echo "---------------------------------------------------------------------------"
 
-rm -rf $BASE_PATH 
+rm -rf $BASE_PATH
 mkdir $BASE_PATH
 
 curl -o $FIRMWARE_PATH $FIRMWARE_URL
@@ -23,3 +23,4 @@ esptool.py --port $ESP8266_PORT erase_flash
 
 esptool.py --port $ESP8266_PORT --baud 460800 write_flash --flash_size=detect 0 $FIRMWARE_PATH
 
+rm -rf $BASE_PATH
